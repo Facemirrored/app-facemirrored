df29ed20b95d0b42559bef0c6ef7daa724f7dfcd
facemirrored <herrmann.bjarne@outlook.de>
2022-07-08 00:08:18 +0200

    setup frontend vues / clean up default setup

---
+++ frontend/src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div
+    style="
+      color: white;
+      margin: 10px;
+      width: 90%;
+      height: 30px;
+      background: black;
+      text-align: center;
+    "
+  >
+    NAV BAR
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "NavBar",
+});
+</script>
+
+<style scoped></style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,16 @@
-<script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
-</script>
-
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="container">
+    <div
+      class="text-center pt-5"
+      style="height: 900px; width: 90%; border: black solid 2px"
+    >
+      HOME CONTENT
+    </div>
+  </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({});
+</script>
